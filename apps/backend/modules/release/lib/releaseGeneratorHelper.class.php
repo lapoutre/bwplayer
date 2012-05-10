9b61b5e008a97b550bb65d7c4bb399edf7b23534
Antoine Belluard <squirhell@gmail.com>
2012-05-10 15:58:43 +0200

    module family + release et fixtures

---
+++ apps/backend/modules/release/lib/releaseGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * release module helper.
+ *
+ * @package    bwrecords
+ * @subpackage release
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class releaseGeneratorHelper extends BaseReleaseGeneratorHelper
+{
+}
