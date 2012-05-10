e85ed72fbb7dcb06443a79e0e3f079ce195b4388
Antoine Belluard <squirhell@gmail.com>
2012-05-10 15:32:59 +0200

    publish asset

---
+++ apps/backend/modules/family/lib/familyGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * family module configuration.
+ *
+ * @package    bwrecords
+ * @subpackage family
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class familyGeneratorConfiguration extends BaseFamilyGeneratorConfiguration
+{
+}
--- lib/filter/doctrine/commentFormFilter.class.php
+++ lib/filter/doctrine/commentFormFilter.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * comment filter form.
+ * Comment filter form.
  *
  * @package    bwrecords
  * @subpackage filter
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class commentFormFilter extends BasecommentFormFilter
+class CommentFormFilter extends BaseCommentFormFilter
 {
   public function configure()
   {
