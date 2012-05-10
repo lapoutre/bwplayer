d1a26faaa52d542adc5ac8f7e3c62aed845001cc
Timothe Roche <timotheroche@gmail.com>
2012-05-10 15:15:46 +0200

    sfGuardAuth backend

---
+++ lib/filter/doctrine/base/BaseFamilyFormFilter.class.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Family filter form base class.
+ *
+ * @package    bwrecords
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BaseFamilyFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'name'         => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'description'  => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'pic'          => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'link'         => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'contact_mail' => new sfWidgetFormFilterInput(),
+      'type_family'  => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'created_at'   => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at'   => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+    ));
+
+    $this->setValidators(array(
+      'name'         => new sfValidatorPass(array('required' => false)),
+      'description'  => new sfValidatorPass(array('required' => false)),
+      'pic'          => new sfValidatorPass(array('required' => false)),
+      'link'         => new sfValidatorPass(array('required' => false)),
+      'contact_mail' => new sfValidatorPass(array('required' => false)),
+      'type_family'  => new sfValidatorPass(array('required' => false)),
+      'created_at'   => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at'   => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+    ));
+
+    $this->widgetSchema->setNameFormat('family_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Family';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'           => 'Number',
+      'name'         => 'Text',
+      'description'  => 'Text',
+      'pic'          => 'Text',
+      'link'         => 'Text',
+      'contact_mail' => 'Text',
+      'type_family'  => 'Text',
+      'created_at'   => 'Date',
+      'updated_at'   => 'Date',
+    );
+  }
+}
