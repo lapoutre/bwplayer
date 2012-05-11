b99d1f4a479d0a75133a6bcb4aed6a0627fbcb55
Antoine Belluard <squirhell@gmail.com>
2012-05-11 15:08:34 +0200

    working tree changes

---
+++ lib/form/doctrine/CommentForm.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Project form base class.
+ *
+ * @package    bwrecords
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormBaseTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class CommentDoctrine extends BaseComment
+{
+  public function configure()
+  {
+      //tinyMCE
+  }
+}
