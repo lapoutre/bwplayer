df83244f69df7545454137fe077283d6768ac0cb
Antoine Belluard <squirhell@gmail.com>
2012-05-14 17:43:38 +0200

    FIX layout + css

--- apps/frontend/modules/news/templates/indexSuccess.php
+++ apps/frontend/modules/news/templates/indexSuccess.php
@@ -1,18 +1,16 @@
-<div id = "wrapper">
-		<article>
-		<h1>News1</h1>
-		<p>
-BWR001 // DECEPTICONS - WE ARE DECEPTICONS EP
-Available on iTunes, Beatport, Juno !
-REMIXES by :
-- WASA3I (HELL)
-- KAZE "VOID CAMP" (SPAIN)
-- THE CHILLERS (FRANCE)
-- LA MUSIQUE DÓRDINATEUR (SPAIN)
-- DEYDEY (COLOMBIA)
-- AIR WANKERS (FRANCE)
-- NOISEPILL (ITALY)
-- VIRGIN CAVALIER (COLOMBIA)
-		</p>
-		</article>
-	</div>
+<div id="article">
+    <h1>News1</h1>
+    <p>
+        BWR001 // DECEPTICONS - WE ARE DECEPTICONS EP
+        Available on iTunes, Beatport, Juno !
+        REMIXES by :
+        - WASA3I (HELL)
+        - KAZE "VOID CAMP" (SPAIN)
+        - THE CHILLERS (FRANCE)
+        - LA MUSIQUE DÓRDINATEUR (SPAIN)
+        - DEYDEY (COLOMBIA)
+        - AIR WANKERS (FRANCE)
+        - NOISEPILL (ITALY)
+        - VIRGIN CAVALIER (COLOMBIA)
+    </p>
+</div>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
-    <?php include_javascripts() ?>
-  </head>
-  <body>
-    <div id="wrapper_outer">
-      <header id = "titre1">
-        <img src="/images/logobw.png" align = "left" width = "250px">
-          BRAIN WASHER RECORDS
-      </header>
-      <div id = "menu">
-
-        <ul id="ul_menu">
-          <li><?php echo link_to('NEWS', 'news') ?></li>
-          <li><?php echo link_to('FAMILY', 'family') ?></li>
-          <li><?php echo link_to('SHOP', 'shop') ?></li>
-          <li><?php echo link_to('CONTACT', 'contact') ?></li>
-        </ul>
-      </div>	
-      <?php echo $sf_content ?>
-      <footer>
-        copyright
-      </footer>
-    </div>
-  </body>
+    <head>
+        <?php include_http_metas() ?>
+        <?php include_metas() ?>
+        <?php include_title() ?>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <?php include_stylesheets() ?>
+        <?php include_javascripts() ?>
+    </head>
+    <body>
+        <div id = "wrapper">
+            <div id = "header" >
+                <img src="/images/logobw.png"  width = "250px">
+                    <div id = "menu">
+                        <ul id="ul_menu">
+                            <li><?php echo link_to('NEWS', 'news') ?></li>
+                            <li><?php echo link_to('FAMILY', 'family') ?></li>
+                            <li><?php echo link_to('SHOP', 'shop') ?></li>
+                            <li><?php echo link_to('CONTACT', 'contact') ?></li>
+                        </ul>
+                    </div>
+            </div>
+            <div id = "main-container">
+                <?php echo $sf_content ?>
+            </div>
+            <div id = "footer">    
+                copyright
+            </div>
+        </div>
+    </body>
 </html>
