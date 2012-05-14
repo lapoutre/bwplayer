4174d4c7fe160160b1c2ababae2732ec5caf7c2b
Antoine Belluard <squirhell@gmail.com>
2012-05-14 18:29:32 +0200

    css

--- apps/frontend/modules/news/templates/indexSuccess.php
+++ apps/frontend/modules/news/templates/indexSuccess.php
@@ -1,5 +1,37 @@
 <div id="article">
-    <h1>News1</h1>
+    <h3>News1</h3>
+    <p>
+        BWR001 // DECEPTICONS - WE ARE DECEPTICONS EP
+        Available on iTunes, Beatport, Juno !
+        REMIXES by :
+        - WASA3I (HELL)
+        - KAZE "VOID CAMP" (SPAIN)
+        - THE CHILLERS (FRANCE)
+        - LA MUSIQUE DÓRDINATEUR (SPAIN)
+        - DEYDEY (COLOMBIA)
+        - AIR WANKERS (FRANCE)
+        - NOISEPILL (ITALY)
+        - VIRGIN CAVALIER (COLOMBIA)
+    </p>
+</div>
+<div id="article">
+    <h3>News1</h3>
+    <p>
+        BWR001 // DECEPTICONS - WE ARE DECEPTICONS EP
+        Available on iTunes, Beatport, Juno !
+        REMIXES by :
+        - WASA3I (HELL)
+        - KAZE "VOID CAMP" (SPAIN)
+        - THE CHILLERS (FRANCE)
+        - LA MUSIQUE DÓRDINATEUR (SPAIN)
+        - DEYDEY (COLOMBIA)
+        - AIR WANKERS (FRANCE)
+        - NOISEPILL (ITALY)
+        - VIRGIN CAVALIER (COLOMBIA)
+    </p>
+</div>
+<div id="article">
+    <h3>News1</h3>
     <p>
         BWR001 // DECEPTICONS - WE ARE DECEPTICONS EP
         Available on iTunes, Beatport, Juno !
