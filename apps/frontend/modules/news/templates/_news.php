390c1bad05c4262d225f308936be2b387f3c1bbe
Antoine Belluard <squirhell@gmail.com>
2012-05-17 00:05:33 +0200

    optim news

--- apps/frontend/modules/news/templates/_news.php
+++ apps/frontend/modules/news/templates/_news.php
@@ -1,5 +1,6 @@
 <div id="article">
-    <h3><?php echo link_to($article->getTitle(), "news_show", $article, array('style' => 'text-decoration: none;color: black;')) ?></h3>
+    <?php $date = str_replace(' ', ' &agrave; ', $article->getCreatedAt()); ?>
+    <div><h3><?php echo link_to($article->getTitle(), "news_show", $article, array('style' => 'text-decoration: none;color: black;')) ?></h3><span id="le-date-string"><?php echo '    le '.$date ?></span></div>
     <p>
     <div id="news-content">
         <?php echo $article->getBody(ESC_RAW) ?>
--- apps/frontend/modules/news/templates/showSuccess.php
+++ apps/frontend/modules/news/templates/showSuccess.php
@@ -1,3 +1,3 @@
-<h3><?php echo $article->getTile(); ?></h3>
+<h3><?php echo $article->getTitle(); ?></h3>
     <img src="/uploads/news/<?php echo $article->getPic(); ?>" >
  <p>   <?php echo $article->getBody(ESC_RAW); ?> </p>
