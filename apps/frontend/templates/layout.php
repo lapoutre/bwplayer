37fe6f03624ce82f989ca0cd6aa0ebc4fe1765d0
Antoine Belluard <squirhell@gmail.com>
2012-05-16 21:53:01 +0200

    design + enum family

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -9,24 +9,29 @@
         <?php include_javascripts() ?>
     </head>
     <body>
+        <?php 
+            $route = sfContext::getInstance()->getRouting()->getCurrentRouteName();
+        ?>
         <div id = "wrapper">
             <div id = "header" >
-                <img src="/images/logobw.png"  width = "250px">
+                <img src="/images/logobw.png"  width = "360px"/>
                     <div id = "menu">
                         <ul id="ul_menu">
-                            <li><?php echo link_to('NEWS', 'news') ?></li>
-                            <li><?php echo link_to('FAMILY', 'family') ?></li>
-                            <li><?php echo link_to('SHOP', 'shop') ?></li>
-                            <li><?php echo link_to('CONTACT', 'contact') ?></li>
+                            <li style="border-left: 2px solid black;border-right: 2px solid black;" <?php if($route == "news") echo 'class="current"' ?>><?php echo link_to('NEWS', 'news') ?></li>
+                            <li <?php if($route == "family") echo 'class="current"' ?>><?php echo link_to('FAMILY', 'family') ?></li>
+                            <li <?php if($route == "shop") echo 'class="current"' ?> style="border-left: 2px solid black;"><?php echo link_to('SHOP', 'shop') ?></li>
+                            <li <?php if($route == "contact") echo 'class="current"' ?> id="even" style="border-right: 2px solid black;border-left: 2px solid black;"><?php echo link_to('CONTACT', 'contact') ?></li>
                         </ul>
                     </div>
+                  
             </div>
             <div id = "main-container">
                 <div id="sf-content">
                     <?php echo $sf_content ?>
                 </div>
             </div>
-            <div id = "footer"> 
+            <div id = "footer">
+                <img id="this-is-a-footer-border"src="/images/border-footer.png"  width="409px" height="50px"/> 
                 <p>
                 copyright
                 </p>
