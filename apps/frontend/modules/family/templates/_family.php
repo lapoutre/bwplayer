c06eba7547c94e13f6a219202031f156f4680a7b
Antoine Belluard <squirhell@gmail.com>
2012-05-17 15:50:20 +0200

    optim css

--- apps/frontend/modules/family/templates/_family.php
+++ apps/frontend/modules/family/templates/_family.php
@@ -1,6 +1,6 @@
 <div id="family_obj">
   <li>
-    <h3><?php //echo $family->getName(); ?><?php echo link_to($family->getName(), "family_show", $family, array('style' => 'text-decoration: none;color: black;')) ?></h3>
+    <div class="h3-gallery"><?php //echo $family->getName(); ?><?php echo link_to($family->getName(), "family_show", $family, array('style' => 'text-decoration: none;color: black;')) ?></div>
 <!--    <img src="/uploads/family/<?php //echo $family->getPic(); ?>" >-->
     <?php echo link_to('<img src="/uploads/family/'.$family->getPic().'">', "family_show", $family) ?>
     <?php // echo link ?>
