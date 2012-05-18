333b333cef9eb44f3e26d40245d6bf3cfcb79a31
Antoine Belluard <squirhell@gmail.com>
2012-05-18 19:37:43 +0200

    ajout form commentaire + prepa shop model

--- apps/frontend/modules/news/templates/_comment.php
+++ apps/frontend/modules/news/templates/_comment.php
@@ -1,8 +1,11 @@
+<div class="comment">
 <?php foreach ($comments as $i => $comment) : ?>
-
+  <?php if($comment->getIsApproved()): ?>
   _______________________________________________________<br>
   <?php echo $comment->getNick(); ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <?php echo $comment->getCreatedAt(); ?><br><br>
   <?php echo $comment->getMessage(); ?><br>
-
+ <?php endif; ?>
 <?php endforeach; ?>
+</div>
+
 
--- lib/form/doctrine/CustomerForm.class.php
+++ lib/form/doctrine/CustomerForm.class.php
@@ -1,22 +1,16 @@
 <?php
 
 /**
- * Contact form.
+ * Customer form.
  *
  * @package    bwrecords
  * @subpackage form
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class ContactForm extends BaseContactForm
+class CustomerForm extends BaseCustomerForm
 {
   public function configure()
   {
-   
-      
-      unset(
-      $this['created_at'], $this['updated_at']
-    );
-   
   }
 }
